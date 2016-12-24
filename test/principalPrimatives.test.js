b11ab68511d732d04375c546441f53b967a281e9
Daniel Somerfield <dsomerfi@thoughtworks.com>
2016-12-23 21:52:41 -0800

    Refactored test for next attribute feature.

--- test/principalPrimatives.test.js
+++ test/principalPrimatives.test.js
@@ -10,15 +10,22 @@ describe("Principal primvitives", ()=>{
         .of(anyResource())
         .if(Principal.attribute('foo').is('bar'))
 
-    it('should support equality matches for attributes', ()=>{
-        let request = {
-            action: 'read',
-            resource: 'my-resource',
-            principal: {
-                foo: "bar"    
-            }
-        };
+    let request = {
+        action: 'read',
+        resource: 'my-resource',
+        principal: {
+            age: 19    
+        }
+    };
 
+    it('should support allow when principal attribute matches policy', ()=>{
+        request.principal.foo = "bar";
         expect(principalPolicy.check(request).code).to.equal(Policy.Result.ALLOWED);
     });
+
+    it('should support deny when principal attribute does not policy', ()=>{
+        request.principal.foo = "baz";
+        expect(principalPolicy.check(request).code).to.equal(Policy.Result.DENIED);
+    });
+
 });
